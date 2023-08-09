6c9e1986dd2d765f8f15c359c60d0c039c7b58da
Bolaji Adeyemi <d.bola1@gmail.com>
2023-08-09 04:21:30 +0100

    sign input

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bolajiadeyemi/validate-hmac
+
+go 1.18
+
+require github.com/alexellis/hmac v1.3.0 // indirect
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/alexellis/hmac"
+)
+
+func main() {
+	input := []byte(`input message from API`)
+	secret := []byte(`so secret`)
+
+	digest := hmac.Sign(input, secret)
+
+	fmt.Printf("Digest: %x\n", digest)
+}
